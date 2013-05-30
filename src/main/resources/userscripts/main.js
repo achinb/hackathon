88b8e836ff03b9d82e618e526163c49c02080f5b
Cyrus Team <cyrusteam@cyruslists.com>
2013-05-30 18:00:00 -0400

    Updated with counter & local storage

--- src/main/resources/userscripts/main.js
+++ src/main/resources/userscripts/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+    var HOST_KEY = '__$BV_ASSISTANT';
+
     if (location.href.indexOf('ugc.bazaarvoice') > 0) {
         return;
     }
@@ -25,4 +27,35 @@ $(document).ready(function() {
 
     var injectableHtml = GM_getResourceText('html');
     $('body').prepend(injectableHtml);
+
+    $(window).on('bv:updateSelectedProducts', function(event, newCount) {
+        $('#counter').text(newCount);
+    });
+
+    function getCurrentDomainProducts() {
+        var existingAsString = localStorage.getItem(HOST_KEY);
+        if (existingAsString == null) {
+            return [];
+        } else {
+            return JSON.parse(existingAsString);
+        }
+    }
+
+    $('ul.assistant-options li').click(function() {
+        var products = getCurrentDomainProducts();
+        var newProducts;
+
+        if (products.length == 0) {
+            newProducts = [location.href];
+        } else {
+            products.push(location.href);
+            newProducts = products;
+        }
+
+        localStorage.setItem(HOST_KEY, JSON.stringify(newProducts));
+        $(window).trigger('bv:updateSelectedProducts', newProducts.length);
+    });
+
+    /* GENERAL USAGE - THE CODEZZ */
+    $(window).trigger('bv:updateSelectedProducts', getCurrentDomainProducts().length);
 });
