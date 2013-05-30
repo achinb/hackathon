880fc872073cfd470e3df6b523f98ffbbc3d0f38
Cyrus Team <cyrusteam@cyruslists.com>
2013-05-30 13:43:01 -0400

    First commit of stuff from Ishan and Jason

---
+++ src/main/resources/userscripts/tampermonkey.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name       BV - In-store Shopping Cart
+// @namespace  https://github.com/achinb/hackathon
+// @version    0.1
+// @description  Captures liked items for storage
+// @run-at       document-end
+// @match      http://*/*
+// @match      https://*/*
+// @require    http://ajax.googleapis.com/ajax/libs/jquery/1.10.0/jquery.min.js
+// @require    file:///Users/cyrus/src/hackathon/src/main/resources/userscripts/main.js
+// @resource   dock.css file:///Users/cyrus/src/hackathon/src/main/resources/css/dock.css
+// ==/UserScript==
